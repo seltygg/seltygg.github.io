79e5516b2ff4f049956af8facc72962dd03d394b
seltygg <sankalpsikchi@arizona.edu>
2025-06-25 14:57:05 -0700

    URL changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// Detect if running in production (GitHub Pages)
+const isProd = process.env.NODE_ENV === 'production';
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
@@ -19,5 +22,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/seltygg.github.io/',
+  base: isProd ? '/seltygg.github.io/docs/' : './',
 }));
